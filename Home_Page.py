0d0a6891b3991972072d8d2e13ec7dbf0419c12e
Omer Raif Ozoglu <111392107+omeraif@users.noreply.github.com>
2026-04-26 22:00:44 -0400

    Update subheader and add new features list

--- Home_Page.py
+++ Home_Page.py
@@ -4,8 +4,8 @@ st.set_page_config(page_title="ReelMatch", page_icon="🎬")
 
 st.title("🎬 Web Development Lab03")
 st.header("CS 1301")
-st.subheader("Web Development Lab03 - Movie Finder and Chatbot")
-st.subheader("Malia Radcliffe - Omer Raif Ozoglu - ")
+st.subheader("Web Development Lab03 - Movie Finder, Chatbot & more...")
+st.subheader("Malia Radcliffe - Omer Raif Ozoglu")
 
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left.
@@ -15,4 +15,6 @@ The following pages are:
 1. **Home Page** – Introduction to our project
 2. **API Page** – A movie finder that uses an external web API
 3. **CineCompass** – A movie-themed AI chatbot powered by Google Gemini
+4. **AI Movie Night Plannera** – Search for a movie and ask AI questions about it using TMDB and Gemini
+5. **Movie Advisor** – A movie-themed AI chatbot powered by Google Gemini
 """)
