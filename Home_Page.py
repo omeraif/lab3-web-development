0a8ab0e8de281427f8adf475803ac64da972ecb0
Omer Raif Ozoglu <111392107+omeraif@users.noreply.github.com>
2026-04-26 22:02:24 -0400

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -16,5 +16,5 @@ The following pages are:
 2. **API Page** – A movie finder that uses an external web API
 3. **CineCompass** – A movie-themed AI chatbot powered by Google Gemini
 4. **AI Movie Night Plannera** – Search for a movie and ask AI questions about it using TMDB and Gemini
-5. **Movie Advisor** – A movie-themed AI chatbot powered by Google Gemini
+5. **Movie Advisor** – A movie recommendation chatbot powered by Google Gemini
 """)
